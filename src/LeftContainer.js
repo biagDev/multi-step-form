afb16b5dfb3070003f7d0fe2b53e61eba3c0066e
Biagio Dev <biagio@biag.dev>
2022-09-19 17:19:30 -0400

    added conditional rendering for stages

--- src/LeftContainer.js
+++ src/LeftContainer.js
@@ -1,11 +1,50 @@
 import TextInput from './TextInput';
-export default function LeftContainer() {
+import ButtonGroup from './ButtonGroup';
+export default function LeftContainer({ stage, setStage }) {
+  function nextStage() {
+    setStage(stage + 1);
+  }
+
+  if (stage === 2) {
+    return (
+      <div className='left-container'>
+        <div className='inner-left-container'>
+          <h2>What's your budget?</h2>
+          <ButtonGroup />
+          <button onClick={nextStage}>Next</button>
+        </div>
+      </div>
+    );
+  }
+
+  if (stage === 3) {
+    return (
+      <div className='left-container'>
+        <div className='inner-left-container'>
+          <h2>Anything else we should know?</h2>
+          <TextInput stage={stage} />
+          <button onClick={nextStage}>Next</button>
+        </div>
+      </div>
+    );
+  }
+
+  if (stage === 4) {
+    return (
+      <div className='left-container'>
+        <div className='inner-left-container'>
+          <h2>Thank you!</h2>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className='left-container'>
-      <div className='content-container'>
+      <div className='inner-left-container'>
         <h2>Apply to work with our agency</h2>
-        <TextInput />
-        <button>Next</button>
+        <TextInput stage={stage} />
+        <button onClick={nextStage}>Next</button>
       </div>
     </div>
   );
