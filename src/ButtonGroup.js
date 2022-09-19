c5f7108c237ca2ec43126ecc63b6b8cdcc8dcb61
Biagio Dev <biagio@biag.dev>
2022-09-19 17:18:55 -0400

    created/completed button group

--- src/ButtonGroup.js
+++ src/ButtonGroup.js
@@ -1,3 +1,39 @@
+import { useState } from 'react';
+
 export default function ButtonGroup() {
-  return <></>;
+  const [selectedButton, setSelectedButton] = useState(0);
+  return (
+    <div className='button-group'>
+      <button
+        style={
+          selectedButton === 1
+            ? { backgroundColor: 'white', color: '#745e5e' }
+            : null
+        }
+        onClick={() => setSelectedButton(1)}
+      >
+        $2k
+      </button>
+      <button
+        style={
+          selectedButton === 2
+            ? { backgroundColor: 'white', color: '#745e5e' }
+            : null
+        }
+        onClick={() => setSelectedButton(2)}
+      >
+        $5k
+      </button>
+      <button
+        style={
+          selectedButton === 3
+            ? { backgroundColor: 'white', color: '#745e5e' }
+            : null
+        }
+        onClick={() => setSelectedButton(3)}
+      >
+        No limit
+      </button>
+    </div>
+  );
 }
