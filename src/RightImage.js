20b36dcd1128057bf0fef45cd65efdfba2f4917e
Biagio Dev <biagio@biag.dev>
2022-09-19 17:19:09 -0400

    added conditional rendering

--- src/RightImage.js
+++ src/RightImage.js
@@ -1,7 +1,40 @@
-export default function RightImage() {
+export default function RightImage({ stage }) {
+  if (stage === 2) {
+    return (
+      <div className='right-image'>
+        <img
+          alt='agency man'
+          src={'https://assets.codepen.io/6060109/form-stage-2.png'}
+        />
+      </div>
+    );
+  }
+  if (stage === 3) {
+    return (
+      <div className='right-image'>
+        <img
+          alt='agency man'
+          src={'https://assets.codepen.io/6060109/form-stage-3.png'}
+        />
+      </div>
+    );
+  }
+  if (stage === 4) {
+    return (
+      <div className='right-image'>
+        <img
+          alt='agency man'
+          src={'https://assets.codepen.io/6060109/form-stage-4.png'}
+        />
+      </div>
+    );
+  }
   return (
     <div className='right-image'>
-      <img src={'https://assets.codepen.io/6060109/form-stage-1.png'} />
+      <img
+        alt='agency women'
+        src={'https://assets.codepen.io/6060109/form-stage-1.png'}
+      />
     </div>
   );
 }
