ae31c1334ba14ae36283d3bb1040bfa7123902e5
Biagio Dev <biagio@biag.dev>
2022-09-20 12:45:45 -0400

    refactored/DRYed code

--- src/LeftContainer.js
+++ src/LeftContainer.js
@@ -1,51 +1,68 @@
 import TextInput from './TextInput';
 import ButtonGroup from './ButtonGroup';
+
+const LABEL_TEXT = [
+  'Apply to work with our agency',
+  "What's your budget?",
+  'Anything else we should know?',
+  'Thank you!',
+];
+
 export default function LeftContainer({ stage, setStage }) {
-  function nextStage() {
-    setStage(stage + 1);
-  }
-
-  if (stage === 2) {
-    return (
-      <div className='left-container'>
-        <div className='inner-left-container'>
-          <h2>What's your budget?</h2>
-          <ButtonGroup />
-          <button onClick={nextStage}>Next</button>
-        </div>
-      </div>
-    );
-  }
-
-  if (stage === 3) {
-    return (
-      <div className='left-container'>
-        <div className='inner-left-container'>
-          <h2>Anything else we should know?</h2>
-          <TextInput stage={stage} />
-          <button onClick={nextStage}>Next</button>
-        </div>
-      </div>
-    );
-  }
-
-  if (stage === 4) {
-    return (
-      <div className='left-container'>
-        <div className='inner-left-container'>
-          <h2>Thank you!</h2>
-        </div>
-      </div>
-    );
+  let content;
+  if (stage === 0) {
+    content = <TextInput stage={stage} />;
+  } else if (stage === 1) {
+    content = <ButtonGroup />;
+  } else if (stage === 2) {
+    content = <TextInput stage={stage} />;
+  } else {
+    content = null;
   }
 
   return (
     <div className='left-container'>
       <div className='inner-left-container'>
-        <h2>Apply to work with our agency</h2>
-        <TextInput stage={stage} />
-        <button onClick={nextStage}>Next</button>
+        <h2>{LABEL_TEXT[stage]}</h2>
+        {content}
+        {stage !== 3 && (
+          <button onClick={() => setStage(stage + 1)}>Next</button>
+        )}
       </div>
     </div>
   );
 }
+
+// if (stage === 2) {
+//   return (
+//     <div className='left-container'>
+//       <div className='inner-left-container'>
+//         <h2>What's your budget?</h2>
+//         <ButtonGroup />
+//         <button onClick={nextStage}>Next</button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// if (stage === 3) {
+//   return (
+//     <div className='left-container'>
+//       <div className='inner-left-container'>
+//         <h2>Anything else we should know?</h2>
+//         <TextInput stage={stage} />
+//         <button onClick={nextStage}>Next</button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// if (stage === 4) {
+//   return (
+//     <div className='left-container'>
+//       <div className='inner-left-container'>
+//         <h2>Thank you!</h2>
+//       </div>
+//     </div>
+//   );
+// }
--- src/RightImage.js
+++ src/RightImage.js
@@ -1,40 +1,13 @@
+const IMG_URLS = [
+  'https://assets.codepen.io/6060109/form-stage-1.png',
+  'https://assets.codepen.io/6060109/form-stage-2.png',
+  'https://assets.codepen.io/6060109/form-stage-3.png',
+  'https://assets.codepen.io/6060109/form-stage-4.png',
+];
 export default function RightImage({ stage }) {
-  if (stage === 2) {
-    return (
-      <div className='right-image'>
-        <img
-          alt='agency man'
-          src={'https://assets.codepen.io/6060109/form-stage-2.png'}
-        />
-      </div>
-    );
-  }
-  if (stage === 3) {
-    return (
-      <div className='right-image'>
-        <img
-          alt='agency man'
-          src={'https://assets.codepen.io/6060109/form-stage-3.png'}
-        />
-      </div>
-    );
-  }
-  if (stage === 4) {
-    return (
-      <div className='right-image'>
-        <img
-          alt='agency man'
-          src={'https://assets.codepen.io/6060109/form-stage-4.png'}
-        />
-      </div>
-    );
-  }
   return (
     <div className='right-image'>
-      <img
-        alt='agency women'
-        src={'https://assets.codepen.io/6060109/form-stage-1.png'}
-      />
+      <img alt='agency women' src={IMG_URLS[stage]} />
     </div>
   );
 }
